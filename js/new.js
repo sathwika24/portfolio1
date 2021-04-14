dfb49c6223c48852e983a04742501cbafc277d39
sathwika24 <sathwikasindhura024@gmail.com>
2021-04-14 16:16:24 +0530

    one

---
+++ js/new.js
@@ -0,0 +1,24 @@
+function git() {
+    window.open("https://serene-elion-8cede0.netlify.app/");
+}
+function shop() {
+    window.open("https://stoic-pike-49ce54.netlify.app");
+}
+function wired() {
+    window.open("https://compassionate-meninsky-417a6d.netlify.app");
+}
+function slack() {
+    window.open("https://condescending-haibt-dda3c0.netlify.app");
+}
+function kilent() {
+    window.open("https://agitated-fermat-aab61b.netlify.app");
+}
+function smashing() {
+    window.open("https://clever-jackson-507b2d.netlify.app");
+}
+function will() {
+    window.open("https://awesome-kare-eaa033.netlify.app");
+}
+function tree() {
+    window.open("https://clever-kilby-50000d.netlify.app");
+}
